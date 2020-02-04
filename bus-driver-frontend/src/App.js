04cadac3a9484769ea97a6f21cd6afe884c445ab
Nick <nicholasinterest1@icloud.com>
2020-02-04 18:05:42 -0500

    NavBar working

--- bus-driver-frontend/src/App.js
+++ bus-driver-frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <p>testing</p>
+      <NavBar />
     </div>
   );
 }
